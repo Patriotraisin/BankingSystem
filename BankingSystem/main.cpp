0ea0a97c25a4b86ae66f444f63e22812d02c7fda
Patriotraisin <205073@learning.gsis.edu.hk>
2020-09-06 19:13:19 +0800

    Initial Commit

---
+++ BankingSystem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BankingSystem
+//
+//  Created by JACK on 6/9/2020.
+//  Copyright © 2020 JACK. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
